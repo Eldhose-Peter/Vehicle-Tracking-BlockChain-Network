3024bc47c93bae902b6d5b2225293759f13a1f64
Eldhose-Peter <eldhosepeter2000@gmail.com>
2022-03-16 18:55:42 +0530

    Added connection profile

--- api-2.0/config/generate-ccp.sh
+++ api-2.0/config/generate-ccp.sh
@@ -5,37 +5,63 @@ function one_line_pem {
 }
 
 function json_ccp {
-    local PP=$(one_line_pem $4)
-    local CP=$(one_line_pem $5)
+    local PP=$(one_line_pem $5)
+    local CP=$(one_line_pem $6)
     sed -e "s/\${ORG}/$1/" \
-        -e "s/\${P0PORT}/$2/" \
-        -e "s/\${CAPORT}/$3/" \
+        -e "s/\${org}/$2/" \
+        -e "s/\${P0PORT}/$3/" \
+        -e "s/\${CAPORT}/$4/" \
         -e "s#\${PEERPEM}#$PP#" \
         -e "s#\${CAPEM}#$CP#" \
         ./ccp-template.json
 }
 
-ORG=1
+ORG=Trans
+org=trans
 P0PORT=7051
 CAPORT=7054
-PEERPEM=../../artifacts/channel/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/tlscacerts/tls-localhost-7054-ca-org1-example-com.pem
-CAPEM=../../artifacts/channel/crypto-config/peerOrganizations/org1.example.com/msp/tlscacerts/ca.crt
+PEERPEM=../../artifacts/channel/crypto-config/peerOrganizations/trans.example.com/peers/peer0.trans.example.com/tls/tlscacerts/tls-localhost-7054-ca-trans-example-com.pem
+CAPEM=../../artifacts/channel/crypto-config/peerOrganizations/trans.example.com/msp/tlscacerts/ca.crt
 
-echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM )" > connection-org1.json
+echo "$(json_ccp $ORG $org $P0PORT $CAPORT $PEERPEM $CAPEM )" > connection-trans.json
 
-ORG=2
+ORG=Manuf
+org=manuf
 P0PORT=9051
 CAPORT=8054
-PEERPEM=../../artifacts/channel/crypto-config/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/tlscacerts/tls-localhost-8054-ca-org2-example-com.pem
-CAPEM=../../artifacts/channel/crypto-config/peerOrganizations/org2.example.com/msp/tlscacerts/ca.crt
+PEERPEM=../../artifacts/channel/crypto-config/peerOrganizations/manuf.example.com/peers/peer0.manuf.example.com/tls/tlscacerts/tls-localhost-8054-ca-manuf-example-com.pem
+CAPEM=../../artifacts/channel/crypto-config/peerOrganizations/manuf.example.com/msp/tlscacerts/ca.crt
 
-echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > connection-org2.json
+echo "$(json_ccp $ORG $org $P0PORT $CAPORT $PEERPEM $CAPEM)" > connection-manuf.json
 
-ORG=3
+ORG=Insur
+org=insur
 P0PORT=11051
 CAPORT=10054
-PEERPEM=../../artifacts/channel/crypto-config/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/tlscacerts/tls-localhost-10054-ca-org3-example-com.pem
-CAPEM=../../artifacts/channel/crypto-config/peerOrganizations/org3.example.com/msp/tlscacerts/ca.crt
+PEERPEM=../../artifacts/channel/crypto-config/peerOrganizations/insur.example.com/peers/peer0.insur.example.com/tls/tlscacerts/tls-localhost-10054-ca-insur-example-com.pem
+CAPEM=../../artifacts/channel/crypto-config/peerOrganizations/insur.example.com/msp/tlscacerts/ca.crt
 
 
-echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > connection-org3.json
+echo "$(json_ccp $ORG $org $P0PORT $CAPORT $PEERPEM $CAPEM)" > connection-insur.json
+
+
+ORG=Owner
+org=owner
+P0PORT=13051
+CAPORT=11054
+PEERPEM=../../artifacts/channel/crypto-config/peerOrganizations/owner.example.com/peers/peer0.owner.example.com/tls/tlscacerts/tls-localhost-11054-ca-owner-example-com.pem
+CAPEM=../../artifacts/channel/crypto-config/peerOrganizations/owner.example.com/msp/tlscacerts/ca.crt
+
+
+echo "$(json_ccp $ORG $org $P0PORT $CAPORT $PEERPEM $CAPEM)" > connection-owner.json
+
+
+ORG=Scrap
+org=scrap
+P0PORT=15051
+CAPORT=12054
+PEERPEM=../../artifacts/channel/crypto-config/peerOrganizations/scrap.example.com/peers/peer0.scrap.example.com/tls/tlscacerts/tls-localhost-12054-ca-scrap-example-com.pem
+CAPEM=../../artifacts/channel/crypto-config/peerOrganizations/scrap.example.com/msp/tlscacerts/ca.crt
+
+
+echo "$(json_ccp $ORG $org $P0PORT $CAPORT $PEERPEM $CAPEM)" > connection-scrap.json
