8c00e1e158b73cd65d3820f47bd4069effa611a1
Eldhose-Peter <eldhosepeter2000@gmail.com>
2022-04-06 14:05:23 +0530

    View vehicle history

--- artifacts/src/github.com/fabcar/javascript/lib/fabcar.js
+++ artifacts/src/github.com/fabcar/javascript/lib/fabcar.js
@@ -261,6 +261,87 @@ class FabCar extends Contract {
         console.info('============= END : changeCarOwner ===========');
     }
 
+    //Dynamic query car by - make
+    async queryCarByMake(ctx,make){
+
+        let queryString = {};
+        queryString.selector = {"make":make}
+        let iterator = await ctx.stub.getQueryResult(JSON.stringify(queryString))
+        let result = await this.getIteratorData(iterator);
+        return JSON.stringify(result);
+    }
+
+    async getIteratorData (iterator){
+        let resultArray =[];
+
+        while(true){
+            let res = await iterator.next();
+
+            //res.value -- contains other metadata
+            //res.value.value -- contains the actual value
+            //res.value.key -- contains the key
+
+            let resJson ={}
+            if(res.value&&res.value.value.toString()){
+                resJson.key = res.value.key;
+                resJson.value = JSON.parse(res.value.value.toString());
+                resultArray.push(resJson);
+            }
+
+            if(res.done){
+                iterator.close();
+                return resultArray;
+            }
+        }
+    }
+    async getAssetHistory(ctx,carNumber){
+        let resultsIterator = await ctx.stub.getHistoryForKey(carNumber);
+        let results = await this.getAllResults(resultsIterator,true);
+
+        return JSON.stringify(results);
+    }
+
+    async getAllResults(iterator, isHistory) {
+        let allResults = [];
+        while (true) {
+          let res = await iterator.next();
+    
+          if (res.value && res.value.value.toString()) {
+            let jsonRes = {};
+            console.log(res.value.value.toString('utf8'));
+    
+            if (isHistory && isHistory === true) {
+                jsonRes.TxId = res.value.tx_id;
+                jsonRes.Timestamp = res.value.timestamp;
+                //jsonRes.IsDelete = res.value.is_delete.toString();
+                try {
+                    jsonRes.Value = JSON.parse(res.value.value.toString('utf8'));
+                } catch (err) {
+                    console.log(err);
+                    jsonRes.Value = res.value.value.toString('utf8');
+                }
+            } else {
+                jsonRes.Key = res.value.key;
+                try {
+                    jsonRes.Record = JSON.parse(res.value.value.toString('utf8'));
+                } catch (err) {
+                    console.log(err);
+                    jsonRes.Record = res.value.value.toString('utf8');
+                }
+            }
+            allResults.push(jsonRes);
+          }
+          if (res.done) {
+            console.log('end of data');
+            await iterator.close();
+            console.info(allResults);
+            return allResults;
+          }
+        }
+    }
+
+
 }
+
 
 module.exports = FabCar;
